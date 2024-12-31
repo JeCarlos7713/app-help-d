50fe240735fb07fe3c73041fe0944dff8c20875e
JeanC777 <jecarlos7713@gmail.com>
2024-12-31 19:34:44 -0300

    feat: listagem dos processos

---
+++ pages/RequestsProcess/requestsProcess.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const RequestsProcess = () => {
+  return (
+    <View>
+        <Text>Central de Chamados</Text>
+    </View>
+  )
+}
+
+export default RequestsProcess
