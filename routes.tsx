6bf97e9bec3d700568eb4757f7ad238611a23c11
JeanC777 <jecarlos7713@gmail.com>
2025-01-02 19:19:59 -0300

    feat: inclusão de cabeçalho nas rotas

--- routes.tsx
+++ routes.tsx
@@ -3,18 +3,18 @@ import home from "./pages/Home/home";
 import Initial from "./pages/Initial/initial";
 import Login from "./pages/Login/login";
 import SuccessProcess from "./pages/SuccessProcess/successProcess";
-import { Image } from "react-native";
-import { PaperProvider } from 'react-native-paper'
+import { Image, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from '@react-navigation/stack'
 import RequestsProcess from "./pages/RequestsProcess/requestsProcess";
+import Header from "./components/Header/Header";
 
 const Tab = createBottomTabNavigator()
 const Stack = createStackNavigator()
 
 const TabNavigator = () => {
     return (
-        <Tab.Navigator screenOptions={{
+        <Tab.Navigator initialRouteName="home" screenOptions={{
             tabBarStyle: {
                 backgroundColor: '#2C2C2C',
                 borderTopColor: '#2C2C2C',
@@ -49,7 +49,7 @@ const Routes = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator initialRouteName="initial">
-                <Stack.Screen name="TabNavigator" component={TabNavigator} options={{ headerShown: false }} />
+                <Stack.Screen name="TabNavigator" component={TabNavigator} options={{header : () => <Header />} }/>
                 <Stack.Screen name="login" component={Login} options={{ headerShown: false }}/>
                 <Stack.Screen name="success" component={SuccessProcess} options={{ headerShown: false }}/>
                 <Stack.Screen name="initial" component={Initial} options={{ headerShown: false }}/>
