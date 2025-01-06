de3a7ae1260f8ad52f3a54bc135b57619b547ba2
JeanC777 <jecarlos7713@gmail.com>
2025-01-06 18:55:30 -0300

    feat: type para props do componente

---
+++ types/PropsStart.ts
@@ -0,0 +1,8 @@
+import { OptionItem } from "./OptionItem"
+
+export type PropsStart = {
+    topicoAjuda : string
+    prioridade  : OptionItem
+    grupo       : OptionItem
+    resumo      : string
+}
