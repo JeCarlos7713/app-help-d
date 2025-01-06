fcb65e2a0206e41bcc2eb97b85d7c2e9ccef18a0
JeanC777 <jecarlos7713@gmail.com>
2025-01-06 20:15:29 -0300

    feat: mensagem de sucesso com id de processo dinâmico

--- pages/SuccessProcess/successProcess.tsx
+++ pages/SuccessProcess/successProcess.tsx
@@ -3,23 +3,19 @@ import AntDesign from "@expo/vector-icons/AntDesign"
 import { useNavigation } from "expo-router"
 import { StyleSheet, Text, View } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
+import { TabNavigatorNavigationProp } from "@/types/RoutesTypes"
 
-interface ProcessProps {
-    route : {}
-}
-
-const SuccessProcess = ({route}: ProcessProps) => {
-    const navigation = useNavigation()
-    const processId = "18777"
+const SuccessProcess = ({route}: any) => {
+    const navigation = useNavigation<TabNavigatorNavigationProp>()
+    const {idProcess} = route.params
   return (
     <SafeAreaView style={styles.container}>
-        <Header />
 
         <View style={styles.containProcess}>
             <AntDesign  name="checkcircleo" color="#42CA3D" size={64} />
             <Text style={styles.text}>
                 Chamado 
-                <Text style={styles.blue}> {processId} </Text> 
+                <Text style={styles.blue} onPress={() => navigation.navigate('TabNavigator')}> {idProcess} </Text> 
                 iniciado com sucesso!
             </Text>
         </View>
