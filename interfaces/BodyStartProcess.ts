3924394f4520da3cc3065897c2ded21e5b88a714
JeanC777 <jecarlos7713@gmail.com>
2025-01-06 19:40:46 -0300

    feat: interface StartProcess

---
+++ interfaces/BodyStartProcess.ts
@@ -0,0 +1,12 @@
+export interface BodyStartProcess {
+    txt_solicitante         : string
+    txt_email               : string
+    txt_data_solic          : string
+    txt_topico_ajuda        : string
+    sl_prioridade           : string
+    ztxt_grupo_atendimento  : string
+    txta_obs_chamado        : string
+    hd_mat_solic            : string
+    hd_grupo_atendimento    : string
+    hd_sla_horas            : string
+}
