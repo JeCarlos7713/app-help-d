a17f28a60474baf394b1b0c80b1bcfa316119c29
JeanC777 <jecarlos7713@gmail.com>
2025-01-06 20:14:50 -0300

    feat: tipagem de roteamento para componente de sucesso

--- types/RoutesTypes.ts
+++ types/RoutesTypes.ts
@@ -1,10 +1,17 @@
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+
 type StackParam = {
-    login : {} | undefined
-    TabNavigator : {} | undefined
+    login           : {} | undefined
+    TabNavigator    : {} | undefined
+    success         : {} | undefined
 }
 
 type LoginNavigationProp = NativeStackNavigationProp<StackParam, 'login'>
 type TabNavigatorNavigationProp = NativeStackNavigationProp<StackParam, 'TabNavigator'>
+type SuccessNavigationProp = NativeStackNavigationProp<StackParam, 'success'>
 
-export {LoginNavigationProp, TabNavigatorNavigationProp}
+export {
+    LoginNavigationProp, 
+    TabNavigatorNavigationProp, 
+    SuccessNavigationProp
+}
