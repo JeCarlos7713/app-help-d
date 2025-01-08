def9018ed738cbcf7a3994905ae6ec6849291729
JeanC777 <jecarlos7713@gmail.com>
2025-01-08 19:28:06 -0300

    feat: scroll dos chamados

--- pages/RequestsProcess/requestsProcess.tsx
+++ pages/RequestsProcess/requestsProcess.tsx
@@ -2,7 +2,7 @@ import Request from '@/components/Request/Request'
 import { RequestsInfos } from '@/types/RequestsInfos'
 import AntDesign from '@expo/vector-icons/AntDesign'
 import React, { useEffect, useState } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, ScrollView } from 'react-native'
 import Process from '@/services/process'
 import useStorage from '@/hooks/useStorage'
 import { useNavigation } from '@react-navigation/native';
@@ -11,31 +11,31 @@ import { LoginNavigationProp } from '@/types/RoutesTypes';
 const RequestsProcess = () => {
   const navigation = useNavigation<LoginNavigationProp>();
   const [requests, setRequests] = useState<RequestsInfos[]>([])
-  
+
   const getDataUser = async () => {
     const dataUser = await useStorage().getData()
     if (dataUser.token) {
       return dataUser.matricula
-    }else{
+    } else {
       navigation.navigate('login')
     }
   }
-  
+
   const setProcessFormmat = async () => {
     const userMat = await getDataUser()
     const process = await new Process(userMat).execute()
-    
+
     if (process.length == 0) {
       throw new Error("Não foi possível carregar os grupos de atendimento solicitados.")
     }
 
     const formattProcess: RequestsInfos[] = process.retorno.map((proc: any) => {
       return {
-        numChamado  : proc.process,
-        status      : proc.status,
-        prioridade  : proc.prioridade,
-        topicoAjuda : proc.topico,
-        atendimento : proc.grupo
+        numChamado: proc.process,
+        status: proc.status,
+        prioridade: proc.prioridade,
+        topicoAjuda: proc.topico,
+        atendimento: proc.grupo
       }
     })
 
@@ -47,19 +47,21 @@ const RequestsProcess = () => {
   }, [])
   return (
     <View style={styles.area}>
-
       <View>
         <Text style={styles.title}> <AntDesign name='barschart' color='#1E1E1E' style={styles.icon} /> Meus Chamados</Text>
       </View>
+      <ScrollView>
 
-      <View style={styles.container}>
-        {
-          requests.length > 0 ?
-            requests.map((request, index) => (
-              <Request key={index} {...request} />
-            )) : <Text style={styles.textChamado}>Ops... Não há chamados abertos</Text>
-        }
-      </View>
+
+        <View style={styles.container}>
+          {
+            requests.length > 0 ?
+              requests.map((request, index) => (
+                <Request key={index} {...request} />
+              )) : <Text style={styles.textChamado}>Ops... Não há chamados abertos</Text>
+          }
+        </View>
+      </ScrollView>
     </View>
   )
 }
